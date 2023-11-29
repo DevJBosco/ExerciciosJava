7982d9f2f392b67d5bbb00cf5c346e0abb08a3a6
João Bosco <131721349+DevJBosco@users.noreply.github.com>
2023-11-29 10:54:34 -0300

    Add files via upload

---
+++ AtvFor/Exercicio02For.java
@@ -0,0 +1,12 @@
+package arrudeia;
+
+public class Exercicio02For {
+public static void main(String[] args) {
+	for (int i = 0; i< 50;i++) {
+		if(i % 2 != 0) {
+			System.out.println(i);	
+		}
+}
+}
+
+	}

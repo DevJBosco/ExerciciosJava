974a884666eda9d13a9629eb79879a8e00a9dc18
João Bosco <131721349+DevJBosco@users.noreply.github.com>
2023-11-29 10:36:18 -0300

    Add files via upload

---
+++ AtvWhile/exercicio02While.java
@@ -0,0 +1,14 @@
+package repeticoes;
+
+public class exercicio02While {
+public static void main(String[] args) {
+		int num = 0;
+		while (num <=100) {
+			if (num %2 == 0) {
+				System.out.println(num);
+			}
+		
+		num++;
+		}
+		}
+}

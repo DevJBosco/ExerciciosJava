1fd25a4c6971cdb2d6d59bdab6305673d21a056b
João Bosco <131721349+DevJBosco@users.noreply.github.com>
2023-12-01 11:39:51 -0300

    Add files via upload

---
+++ AtvArray/Exercicio05Array.java
@@ -0,0 +1,14 @@
+package arrudeia;
+import java.util.Scanner;
+public class Exercicio05Array {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);		
+ 	double[] a = {5.2 , 4.2 , 2.5 , 5.7 , 3.9 , 2.8};
+ 	
+	for (int x=0; x<a.length; x++) {
+	System.out.print(a[x] + " ");
+	
+	}
+	
+		}
+	}

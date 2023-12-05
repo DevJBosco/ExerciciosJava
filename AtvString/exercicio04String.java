6f2f7807c69a6c75f57521766747755d6a2a368b
João Bosco <131721349+DevJBosco@users.noreply.github.com>
2023-12-05 10:58:10 -0300

    Add files via upload

---
+++ AtvString/exercicio04String.java
@@ -0,0 +1,12 @@
+package ReceitaFederal;
+
+public class exercicio04String {
+public static void main(String[] args) {
+	String[] array = {"a","vida","é","bela"};
+	for(int i = 0; i<4; i++) {
+		System.out.print(array[i] + " ");
+	}
+
+
+}
+}
---
+++ AtvString/exercicio05String.java
@@ -0,0 +1,12 @@
+package ReceitaFederal;
+
+public class exercicio05String {
+public static void main(String[] args) {
+	String[] array = {"a","vida","é","bela"};
+	for(int i = 3; i>=0; i--) {
+		System.out.print(array[i] + " ");
+	}
+
+
+}
+}
